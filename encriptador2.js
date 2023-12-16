081654be544ae5a2e16444042451f18506f71b7c
Franli Marrero <franlimarrero17@gmail.com>
2023-12-15 19:36:52 -0500

    first commit

---
+++ encriptador2.js
@@ -0,0 +1,14 @@
+document.getElementById("copy").addEventListener("click", function() {
+    // Seleccionar el texto del textarea
+    var textarea = document.getElementById("msgResult");
+    textarea.select();
+    
+    // Copiar el texto al portapapeles
+    document.execCommand('copy');
+    
+    // Desseleccionar el texto
+    textarea.setSelectionRange(0, 0);
+
+    // Mostrar un mensaje o realizar alguna acción adicional
+    alert("Texto copiado al portapapeles: " + textarea.value);
+});
